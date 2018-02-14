2ed6059c7bce287ab8114624d2af9bdf0663092d
tejbhangu <tejbhangu432@gmail.com>
2018-02-13 21:48:08 -0700

    new

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+            //GET: Details?hardware=[value]
+        public ActionResult Details(string hardware)
+            {
+            ViewBag.Hardware = hardware;
+                return View();
+            }
+    }
+}
